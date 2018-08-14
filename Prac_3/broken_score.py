a4c23f616838d6ade81d7a5acd3a7bfc7ab0f44e
ryanclark532 <42229549+ryanclark532@users.noreply.github.com>
2018-08-14 15:33:19 +1000

    Add files via upload

--- Prac_3/broken_score.py
+++ Prac_3/broken_score.py
@@ -1,6 +1,16 @@
 def main():
-    num=get_score("give me a score")
+    num=get_score()
     print(num)
 def get_score(message):
-    score=input(message)
+    score=input("score: ")
+    if score>85:
+        r="HD"
+    elif score>75:
+        r="D"
+    elif score>65:
+        r="C"
+    elif score>50:
+       r="P"
+    else:
+        r="F"
     return score
