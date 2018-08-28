b68fa7e0902befdc7e0f87d30d50bebc32ef6be5
ryanclark532 <42229549+ryanclark532@users.noreply.github.com>
2018-08-28 14:44:07 +1000

    Delete hex_colours.py

--- hex_colours.py
+++
@@ -1,8 +0,0 @@
-colours = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb",
-           "AntiqueWhite2": "#eedfcc", "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378"}
-colourname=input("Name of colour: ")
-while colourname !="":
-    if colourname in colours:
-        print("colour code for {} is {}".format(colourname,colours[colourname]))
-    else:
-        print("invalid colour name")
